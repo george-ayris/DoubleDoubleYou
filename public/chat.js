a47c9315c036078b075f722a812f52462514cd32
George Ayris <george.ayris@huddle.com>
2015-07-01 15:36:50 +0100

    Clears input after send and some renaming

--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,11 @@
 window.onload = function() {
   var messages = [];
   var socket = io.connect('http://192.168.1.22:3700');
-  var field = document.getElementById('field');
+  var messageInput = document.getElementById('field');
   var sendButton = document.getElementById('send');
   var content = document.getElementById('content');
 
   socket.on('message', function(data) {
-    console.log('Message received: ' + data.message);
     if(data.username) {
       content.innerHTML += data.username + ': ' + data.message + '<br />';
     } else {
@@ -15,12 +14,11 @@ window.onload = function() {
   });
 
   sendButton.onclick = function() {
-    var text = field.value;
-    console.log('Sending message: ' + text);
     socket.emit('send', {
       username: username,
-      message: text
+      message: messageInput.value
     });
+    messageInput.value = "";
   }
 
   var username = prompt('Please enter your name:', '');
