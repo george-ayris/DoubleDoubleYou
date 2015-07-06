f808e48a6904b212c67c7cf9c685e8bb55647396
George Ayris <george.ayris@huddle.com>
2015-07-06 23:28:58 +0100

    Disconnect tests

--- __tests__/chatServerTest.js
+++ __tests__/chatServerTest.js
@@ -1,8 +1,8 @@
 jest.dontMock('../chatServer');
+var chatServer = require('../chatServer');
 
 describe('send', function() {
   it('forwards on the data', function() {
-    var chatServer = require('../chatServer');
     var mockEmit = jest.genMockFunction();
     var sockets = { emit: mockEmit };
     var data = { message: 'Test message' };
@@ -10,7 +10,21 @@ describe('send', function() {
     chatServer.send(data, sockets);
 
     expect(mockEmit.mock.calls.length).toBe(1);
-    expect(mockEmit.mock.calls[0][0]).toBe('message') // first arg
+    expect(mockEmit.mock.calls[0][0]).toBe('message'); // first arg
     expect(mockEmit.mock.calls[0][1]).toBe(data); // second arg
+  });
+});
+
+describe('disconnect', function() {
+  it('broadcasts a disconnect message', function() {
+    var mockEmit = jest.genMockFunction();
+    var socket = { broadcast: { emit: mockEmit }};
+    var username = "Gumbo";
+    socket.username = username;
+    chatServer.disconnect(socket);
+
+    expect(mockEmit.mock.calls.length).toBe(1);
+    expect(mockEmit.mock.calls[0][0]).toBe('message');
+    expect(mockEmit.mock.calls[0][1]).toEqual({ message: username + ' left the chat' });
   });
 });
--- chatServer.js
+++ chatServer.js
@@ -3,15 +3,15 @@ var register = function(data, socket) {
   socket.username = data.username;
   //users.push(data.username);
   socket.broadcast.emit('message', { message: data.username + ' is now online' });
-}
+};
 
 var send = function(data, sockets) {
   sockets.emit('message', data);
-}
+};
 
 var disconnect = function(socket) {
   socket.broadcast.emit('message', { message: socket.username + ' left the chat' });
-}
+};
 
 module.exports = {
   register: register,
